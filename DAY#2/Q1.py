b26576ec883d7de6777e88c7ac587f5b632cb58c
Rowyda Ramadan <76496105+Rowyda020@users.noreply.github.com>
2023-03-06 05:36:49 +0200

    Add files via upload

---
+++ DAY#2/Q1.py
@@ -0,0 +1,10 @@
+#Write a function that accepts two arguments (length, start) to generate an array of a specific length filled with integer numbers increased by one from start.
+def func(l,s):
+    for i in range (l):
+        s+=i
+        ls.append(i)
+ls=[]
+l = int(input()) 
+s = int(input()) 
+func (l,s)
+print(ls)
